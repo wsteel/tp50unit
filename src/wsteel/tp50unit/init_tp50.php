4698ce8c17d41f95afa52aebb4eea5b2b311cc1f
Wang.Gang <glogger@gmail.com>
2018-09-18 18:45:24 +0800

    include extend path

--- src/wsteel/tp50unit/init_tp50.php
+++ src/wsteel/tp50unit/init_tp50.php
@@ -8,5 +8,6 @@ use think\App;
 
 define('ROOT_PATH', '/../../../../../../');
 define('APP_PATH', __DIR__ . '/../../../../../../application/');
+define('EXTEND_PATH', __DIR__.'/../../../../../../extend/');
 require __DIR__ . '/../../../../../../thinkphp/base.php';
 App::initCommon();
